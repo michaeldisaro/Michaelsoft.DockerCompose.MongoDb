edf41475a51060c8c1e64604d84f73bcb175970e
michaeldisaro <michael.disaro@gmail.com>
2021-02-12 16:27:32 +0100

    fixed rm init

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 . ./source-vars.sh
 docker container stop "$MONGO_CONTAINER_NAME" > /dev/null 2> /dev/null
 docker container rm "$MONGO_CONTAINER_NAME" > /dev/null 2> /dev/null
-sudo rm ./init-mongo.js
+sudo rm -rf ./init-mongo.js
 echo "db=db.getSiblingDB('$MONGO_INITDB_DATABASE');db.createUser({user:'$MONGO_INITDB_DATABASE_USER_USERNAME',pwd:'$MONGO_INITDB_DATABASE_USER_PASSWORD',roles:[{role:'$MONGO_INITDB_DATABASE_USER_ROLES',db:'$MONGO_INITDB_DATABASE'}]});" > ./init-mongo.js
 sudo rm -rf /"$MONGO_CONTAINER_NAME"
 sudo mkdir -p /"$MONGO_CONTAINER_NAME"/data
